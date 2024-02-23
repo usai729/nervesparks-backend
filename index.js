4a011fdd62b14a47f317e39dee6122f292376b5b
Sai Uttarkar <usai7261@gmail.com>
2024-02-23 08:04:39 +0530

    Format README.md

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const _ = require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const _ = require("dotenv").config();
 
-const connectToDatabase = require('./config/DBConnection');
-const Routes = require('./routes/Routes');
+const connectToDatabase = require("./config/DBConnection");
+const Routes = require("./routes/Routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,19 +11,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(async (req, res, next) => {
-	try {
-		const client = await connectToDatabase();
-		req.dbClient = client;
+  try {
+    const client = await connectToDatabase();
+    req.dbClient = client;
 
-		next();
-	} catch (e) {
-		console.log(e);
+    next();
+  } catch (e) {
+    console.log(e);
 
-		res.status(500).json({ msg: 'err/Internal Server Error' });
-	}
+    res.status(500).json({ msg: "err/Internal Server Error" });
+  }
 });
-app.use('/api', Routes);
+app.use("/api", Routes);
+app.get("/", (req, res) => {
+  return res.send("Hello, Nervespark!");
+});
 
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}@http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}@http://localhost:${PORT}`);
 });
